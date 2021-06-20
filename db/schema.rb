4dc9c8ed8e17aa7828e29cb2e1a06632ce599781
JohannesHupfeld <j.hupfeld@gmail.com>
2021-06-19 21:27:51 -0400

    added updated table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_06_19_134403) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
