214be80d42420ceb38aeb160ff39ed22db5657c7
JohannesHupfeld <j.hupfeld@gmail.com>
2021-06-20 23:52:37 -0400

    added has secure password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   #has_many :categories, through: :recipes
   has_many :comments
   has_many :commented_recipes, through: :comments, source: :recipe # The odin project 
+  has_secure_password # Authenticate, validate passowrd & password confirmation
 
 end
 
