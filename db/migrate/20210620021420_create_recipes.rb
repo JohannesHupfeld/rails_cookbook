b087f33ce999706059ca287f72eb4b002236ce08
JohannesHupfeld <j.hupfeld@gmail.com>
2021-06-20 21:43:34 -0400

    changed instructions and ingredients from plural to singular

--- db/migrate/20210620021420_create_recipes.rb
+++ db/migrate/20210620021420_create_recipes.rb
@@ -2,8 +2,8 @@ class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.text :instructions
-      t.text :ingredients
+      t.text :instruction
+      t.text :ingredient
       t.integer :user_id
       t.integer :category_id
 
