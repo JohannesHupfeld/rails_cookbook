9583ec60a01e5c08f141d4605388491b17220177
JohannesHupfeld <j.hupfeld@gmail.com>
2021-06-20 21:44:10 -0400

    added modified recipe table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 2021_06_20_032329) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.text "instructions"
-    t.text "ingredients"
+    t.text "instruction"
+    t.text "ingredient"
     t.integer "user_id"
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
