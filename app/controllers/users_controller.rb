f29682221cb3447138b0b794ed7775502906627d
JohannesHupfeld <j.hupfeld@gmail.com>
2021-06-22 06:28:44 -0400

    added create action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id #Log user in
+      redirect_to @user #Redirect to show page
+    else
+      render :new
+    end
   end
 
   def show
   end 
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
