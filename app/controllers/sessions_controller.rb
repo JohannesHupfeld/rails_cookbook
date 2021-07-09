b694146ea43ee8f8e1986dc828233c9b61168b63
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-09 15:47:11 -0400

    added new action and added some notes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,13 @@ class SessionsController < ApplicationController
   def home
   end
 
+  #logout
   def destroy
     session.clear
     redirect_to root_path
+  end
+
+  #login
+  def new
   end
 end
