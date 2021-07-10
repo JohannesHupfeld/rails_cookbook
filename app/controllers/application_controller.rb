f5a2321ec0cf5925b2eb32defad5bb2d47ebe91c
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-09 23:34:32 -0400

    added notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  
+
+  #Gives access to helper methods in the  views 
+  helper_method(:current_user, :logged_in?)
+
+  private 
   def current_user
     #If current user is already defined then itll equal @ current_user otherwise itll find by id in the users table
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id] #The if prevents calls to the database if no need
