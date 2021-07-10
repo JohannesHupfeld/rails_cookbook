b09dbcb66bb573583e2fc34c715d28a63af24e75
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-10 13:00:33 -0400

    created index and new action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :redirect_if_not_logged_in
+
+  def index
+    @comments = Comment.all 
+  end
+
+  def new
+    @comment = Comment.new
+  end
 end
