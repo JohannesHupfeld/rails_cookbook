e6df4b32fcadde037cf6149a3d1b06e44f8723eb
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-09 21:35:22 -0400

    created create action for login in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,4 +11,16 @@ class SessionsController < ApplicationController
   #login
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user) # user_path creates a URL --> "/users/#{user.id}"
+    else
+      flash[:message] = "Invalid Username or Password"
+      redirect_to login_path
+    end
+  end
+
 end
