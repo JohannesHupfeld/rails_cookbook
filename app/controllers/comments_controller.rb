ca1af9d5236ef5886c93089e3c6d00803cc3c644
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-10 13:03:36 -0400

    created create, show, edit, and update action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,4 +8,26 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
+
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      redirect_to comments_path
+    else
+      render :new
+    end
+  end
+
+  def show 
+    @comment = Comment.find_by(id: params[:id])
+  end
+
+  def edit
+    @comment = Comment.find_by(id: params[:id])
+  end
+
+  def update
+    @comment = Comment.find_by(id: params[:id])
+  end
+  
 end
