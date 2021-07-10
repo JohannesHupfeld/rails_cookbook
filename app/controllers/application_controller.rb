9f98a8bfeb5c09e0131ce3581c199222191e9002
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-09 22:57:46 -0400

    created # helpers current_user, logged_in?, & redirect_to_if_not_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  
+  def current_user
+    #If current user is already defined then itll equal @ current_user otherwise itll find by id in the users table
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id] #The if prevents calls to the database if no need
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def redirect_if_not_logged_in
+    redirect_to '/' if !logged_in?
+  end
 end
