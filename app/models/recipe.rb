bec1520d6cdc722cba5bee1e3efa1faf47d5580e
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-11 12:50:05 -0400

    added belongs to category

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user # Macro gives us two methods, writer & reader
-  # belongs_to :category
+  belongs_to :category
   has_many :comments
   has_many :users, through: :comments # Gives us the writer method for the plural 
 end
