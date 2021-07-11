fd7fa6820ad7bd92675ec8e0e6826c6db0a810b8
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-10 22:19:02 -0400

    created helper for recipe views

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,19 @@
 module RecipesHelper
+  def index_display_header
+    if @user
+      content_tag(:h1, "#{@user.name}'s Recipes:")
+    else
+      content_tag(:h2, "All Recipes")
+    end
+  end
+
+  def display_recipes
+    if @user.recipes.empty?
+      tag.h2(link_to('No recipes yet - write a recipe here', new_recipe_path))
+    else
+      user = @user == current_user ? 'Your' : "#{@user.name}'s"
+      content_tag(:h2, "#{user} #{pluralize(@user.recipes.count, 'Recipe')}:")
+
+    end
+  end
 end
