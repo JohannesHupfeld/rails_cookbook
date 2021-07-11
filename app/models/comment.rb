ab167b1d4dd87cc81f91be5d08d6620d783ffa0a
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-11 14:53:33 -0400

    added validates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :recipe    
 
   validates :content, presence: true 
+  validates :post, uniqueness: {scope: :user_id, message: "may only have one comment per user"}
 end
